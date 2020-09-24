6a9884525d9b6f0c2639c1d8e48d5437a16cff48
Aidan Middleton <aidanemiddleton@gmail.com>
2020-09-24 09:17:57 -0700

    first commit

---
+++ halfSorted.js
@@ -0,0 +1,23 @@
+// Write a function that takes in an array, and sorts the first
+// half of the array in ascending order, and the second half 
+// of the array in descending order
+
+// you may use methods for this. 
+
+let arr1 = [4, 78, 54, 34, 2, 8, 45, 77, 99, 23, 567, 6, 7]
+
+let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45]
+
+
+const halfSorted = function(arr) {
+
+};
+
+
+console.log(halfSorted(arr1));
+
+//results --> [2, 4, 8, 34, 54, 78, 567, 99, 77, 45, 23, 7, 6]
+
+console.log(halfSorted(arr2));
+
+//results --> [3, 4, 56, 58, 65, 98, 90, 68, 45, 34, 12]
---
+++ insertionSort.js
@@ -0,0 +1,16 @@
+// write a insertion sort algorithm based on the video shown in class.
+// should take in an array, and sort it into ascending order using 
+// insertion sorting logic
+
+let arr1 = [4, 78, 54, 34, 2, 8, 45, 77, 99, 23, 567, 6, 7];
+
+let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45];
+
+
+const insertionSort = function(arr) {
+
+};
+
+console.log(insertionSort(arr1));
+
+console.log(insertionSort(arr2));
